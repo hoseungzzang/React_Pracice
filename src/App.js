fe7d496aa5b466861da40be3184174ae9218e634
hoseungzzang <wnghtmd99@naver.com>
2022-10-20 20:22:20 +0900

    props, 이벤트핸들러 학습

--- src/App.js
+++ src/App.js
@@ -5,13 +5,29 @@ import {useState} from 'react';
 
 function App() {
 
-  let [title,changeTitle] = useState(['312','123','212']);
+  let [title,changeTitle] = useState(['남자크토 추천','강남 맛집','파이썬']);
   let posts = '가나다라마';
   let color1 = {color : 'blue' , fontSize : '30px'};
+  let [modal, setModal] = useState(false);
+  let [sliceTitle,setSliceTitle] = useState(0);
+  let [addTitle,setAddTitle] = useState('');
   const onIncrease = () => {
     changeTitle(['바뀐제목','2','3']);
   }
-  let [count,plusCount] = useState(0);
+  
+  let arr2 = [];
+  title.map(function(){
+    arr2.push(0);
+  })
+  let [count,plusCount] = useState(arr2);
+
+
+  function plusC(index){
+    let arr = [...count] ;
+    arr[index] = count[index]+1;
+    plusCount(arr);
+  }
+
   function cTitle(){
     let newArray = [...title] ;
     newArray[0] = '여자코트추천';
@@ -23,42 +39,51 @@ function App() {
     newArray.sort();
     changeTitle(newArray);
   }
+
+  function enterTitle(){
+    let newArray = [...title] ;
+    newArray.push(addTitle);
+    changeTitle(newArray);
+  }
+
   return (
     <div className="App">
      <div className="black-nav">
       <div>개발 Blog</div>
       <button onClick={sort}>정렬</button>
      </div>
-     <div className ="list">
-      <h3>{title[0]} <span onClick={()=>{plusCount(count+1)} }>★</span> {count}</h3>
+
+     {
+      title.map(function(a,i){
+        return (
+          <div className ="list">
+      <h3 onClick={()=>{modal==true?sliceTitle==i?setModal(false):'' :setModal(true);setSliceTitle(i) }}>{title[i]}</h3><span onClick={()=>{plusC(i)} }>★</span> {count[i]}
       <p>2월 18일 발행</p>
-      <button onClick={cTitle}>+1</button>
-      <hr/>
-     </div>
-     <div className ="list">
-      <h3>{title[1]}</h3>
-      <p>2월 18일 발행</p>
+
       <button onClick={onIncrease}>+1</button>
       <hr/>
      </div>
-     <div className ="list">
-      <h3>{title[2]}</h3>
-      <p>2월 18일 발행</p>
-      <button onClick={onIncrease}>+1</button>
-      <hr/>
-     </div>
-      <Modal></Modal>
+        )
+      })
+     }
+     {
+      modal == true ?  <Modal title = {title} titleIndex = {sliceTitle} se={cTitle}/> : null
+     }
+      <input type="text" id="message" onChange={(e)=>{setAddTitle(e.target.value);}}></input><button onClick={enterTitle}>글 발행</button>
     </div>
   );
 }
 //Component 
-function Modal(){
+function Modal(props){
+
   return (
     <div className="modal">
-      <h2>제목</h2>
+      <h4>{props.title[props.titleIndex]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button onClick={props.se}>버튼</button>
     </div>
   )
 }
+
 export default App;
